ab891d78ad77d77cebde3cf5e1b80b8baffd0810
nezumin <nezumin@protonmail.com>
2019-11-01 12:41:25 +0700

    Added widechar warning

--- inc/curses_window.h
+++ inc/curses_window.h
@@ -1,6 +1,12 @@
 #ifndef NEZUMIN_20191031_1813
 #define NEZUMIN_20191031_1813
 
+/* WARNING
+ * The preprocessor directive bellow MUST be declared
+ * before you include ncurses. Otherwise the wide-char
+ * functions will not be loaded!
+ */
+
 #define _XOPEN_SOURCE_EXTENDED
 
 #include "generator.h"
