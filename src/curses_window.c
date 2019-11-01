646009f821a89a9ea522190bc5c79c7961a51701
nezumin <nezumin@protonmail.com>
2019-11-01 15:34:51 +0700

    ui aesthetics

--- src/curses_window.c
+++ src/curses_window.c
@@ -4,33 +4,48 @@
 
 void draw_chart_info(Chart *chart, int mode, int y, int x)
 {
-	WINDOW *winbox = newwin(19, 33, y, x);
-	box(winbox, 0, 0);
-	wrefresh(winbox);	// prevents box outline misallignment
-	
-	const char *mode_label[] =
+	static const char *mode_label[] =
 	{
 		"Elements (Modern)", "Elements (Traditional)", "Planetary",
 		"Zodiac (Agrippa)", "Zodiac (Gerard of Cremona)"
 	};
 	
-	wattron(winbox, A_UNDERLINE);
+	WINDOW *color_label = newwin(1, 33, y, x);
+	
+	if (mode == ELEMENT_M || mode == ELEMENT_T)
+		wattron(color_label, A_REVERSE | COLOR_PAIR(GREEN));
+	else if (mode == PLANETARY)
+		wattron(color_label, A_REVERSE | COLOR_PAIR(MAGENTA));
+	else if (mode == ZODIAC_A)
+		wattron(color_label, A_REVERSE | COLOR_PAIR(YELLOW));
+	else
+		wattron(color_label, A_REVERSE | COLOR_PAIR(BLUE));
+	
+	for (int i = 0; i < 33; ++i)
+		mvwaddch(color_label, 0, i, ' ');
+	
 	int mid_x = (33 - strlen(mode_label[mode])) / 2;
-	mvwaddstr(winbox, 1, mid_x, mode_label[mode]);
-	wattroff(winbox, A_UNDERLINE);
+	mvwaddstr(color_label, 0, mid_x, mode_label[mode]);
+	
+	// figure names and correspondence
+	WINDOW *winbox = newwin(18, 33, y+1, x);
+	box(winbox, 0, 0);
+	wrefresh(winbox);	// prevents box outline misallignment
 	
 	for (int i = 0; i < 16; i++)
 	{
 		Figure *fgr = chart->figures[i];
 		Virtue *vrt = fgr->virtues[mode];
 		
-		mvwprintw(winbox, i+2, 1, "%2d. ", i+1);
+		mvwprintw(winbox, i+1, 1, "%2d ", i+1);
 		wattron(winbox, COLOR_PAIR(vrt->color));
-		mvwprintw(winbox, i+2, 5, "%-12s %s %s", fgr->abbr, vrt->symbol, vrt->name);
+		mvwprintw(winbox, i+1, 4, "%-14s %s %s", fgr->name, vrt->symbol, vrt->name);
 		wattroff(winbox, COLOR_PAIR(vrt->color));
 	}
 	
+	wrefresh(color_label);
 	wrefresh(winbox);
+	delwin(color_label);
 	delwin(winbox);
 }
 
