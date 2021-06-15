7b0fcb656530e269f514d02f33ac3116cb8d9023
nezumin <nezumin@protonmail.com>
2021-06-15 15:32:04 +0700

    Form layout change

--- godot/scripts/ReadingForm.gd
+++ godot/scripts/ReadingForm.gd
@@ -5,17 +5,17 @@ signal input_cancel
 
 func _ready():
 	for i in range(1, 13):
-		$House/Number.add_item(str(i))
+		$Panel/House/Number.add_item(str(i))
 	
-	$Chart/House.connect("toggled", self, "on_house_toggled")
-	$Continue.connect("pressed", self, "on_continue_pressed")
-	$Cancel.connect("pressed", self, "on_cancel_pressed")
+	$Panel/Chart/House.connect("toggled", self, "on_house_toggled")
+	$Panel/Continue.connect("pressed", self, "on_continue_pressed")
+	$Panel/Cancel.connect("pressed", self, "on_cancel_pressed")
 
 func on_house_toggled(is_on):
 	if (is_on):
-		$House/Number.disabled = false
+		$Panel/House/Number.disabled = false
 	else:
-		$House/Number.disabled = true
+		$Panel/House/Number.disabled = true
 
 func on_continue_pressed():
 	emit_signal("input_ok")
