49843ff2f5edb2b118cce7a75cb7faa1d19d25a0
nezumin <nezumin@protonmail.com>
2021-06-24 15:52:59 +0700

    Figure selection screen QoL

--- godot/scripts/FigureSelection.gd
+++ godot/scripts/FigureSelection.gd
@@ -15,6 +15,10 @@ func _ready():
 		button.set_figure(figure)
 		button.connect("selected", self, "on_figure_selected")
 		$Selection.add_child(button)
+	
+	$Selected/Clear.connect("pressed", self, "on_figure_clear")
+	$Selected/Remove.connect("pressed", self, "on_figure_removed")
+	$Selected/OK.connect("pressed", self, "on_figure_ok")
 
 func set_selection(array):
 	selection = array
@@ -23,11 +27,45 @@ func on_figure_selected(figure):
 	selection.append(figure)
 	var selected = Clickable.instance()
 	selected.set_figure(figure)
-	$Selected.add_child(selected)
+	$Selected/Figures.add_child(selected)
 	
+	_reenable_buttons()
+
+func on_figure_removed():
+	var figures = $Selected/Figures
+	if (figures.get_child_count() > 0):
+		var node = figures.get_child(figures.get_child_count() - 1)
+		node.queue_free()
+		selection.pop_back()
+	
+	_reenable_buttons()
+
+func on_figure_clear():
+	selection.clear()
+	var figures = $Selected/Figures
+	if (figures.get_child_count() > 0):
+		for child in figures.get_children():
+			figures.remove_child(child)
+			child.queue_free()
+	
+	_reenable_buttons()
+
+func _reenable_buttons():
 	if (selection.size() == 4):
+		$Selected/OK.disabled = false
 		for child in $Selection.get_children():
 			child.disabled = true
+	else:
+		$Selected/OK.disabled = true
 		
-		emit_signal("done_selecting")
+		if (selection.size() > 0):
+			$Selected/Clear.disabled  = false
+			$Selected/Remove.disabled = false
+		
+		if ($Selection.get_child(0).disabled):
+			for child in $Selection.get_children():
+				child.disabled = false
 
+func on_figure_ok():
+	if (selection.size() == 4):
+		emit_signal("done_selecting")
